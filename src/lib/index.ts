dc1abc2f8efe112bcc9a5f1fd8ffabd1623e62b8
Rickson Oliveira <rickson.patrick13@gmail.com>
2023-09-26 23:00:43 -0300

    fix: adjust project folders

--- src/lib/index.ts
+++ src/lib/index.ts
@@ -1,4 +1,4 @@
-export { default as Button } from './button/Button.svelte';
-export { default as Typography } from './typography/Typography.svelte';
-export * from './types';
-export * from './tokens';
+export { default as Button } from './components/button/Button.svelte';
+export { default as Typography } from './components/typography/Typography.svelte';
+export * from './utils/types';
+export * from './theme/tokens';
--- src/lib/types/text.d.ts
+++
@@ -1,3 +0,0 @@
-export type FontSizes = '0.5rem' | '0.625rem' | '0.75rem' | '0.875rem' | '1rem' | '1.125rem' | '1.25rem' | '1.375rem' | '1.5rem' | '1.625rem' | '1.75rem' | '1.875rem' | '2rem' | '2.125rem' | '2.25rem';
-export type FontWeight = '400' | '500' | '600';
-export type TextAlign = 'center' | 'end' | 'start' | 'justify';
