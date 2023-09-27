d0b479aa0e5c353d8d56f4d3227db915e65bd2a5
Rickson Oliveira <rickson.patrick13@gmail.com>
2023-09-26 22:30:39 -0300

    bump: planeja-ui-svelte@0.0.7

---
+++ src/shared/theme/types/colors.d.ts
@@ -0,0 +1,1 @@
+export type Colors = 'blue-50' | 'blue-100' | 'blue-200' | 'blue-300' | 'blue-400' | 'blue-500' | 'blue-600' | 'blue-700' | 'blue-800' | 'gray-100' | 'gray-200' | 'gray-300' | 'gray-400' | 'gray-500' | 'white';
---
+++ src/shared/tokens/font.d.ts
@@ -0,0 +1,3 @@
+import type { FontSizes } from '../theme/types/text.js';
+export declare const FONT_SIZES: FontSizes[];
+export declare const FONT_WEIGHT: string[];
