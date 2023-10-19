032b3212336ed6d0496cbe0f50af923c4665ab76
Rickson Oliveira <rickson.patrick13@gmail.com>
2023-10-18 22:05:01 -0300

    docs: ItemsParPage.mdx

--- src/lib/components/items-per-page/ItemsPerPage.stories.ts
+++ src/lib/components/items-per-page/ItemsPerPage.stories.ts
@@ -5,19 +5,25 @@ import ItemsPerPage from './ItemsPerPage.svelte';
 const meta = {
 	title: 'Components/ItemsPerPage',
 	component: ItemsPerPage,
-	tags: ['autodocs'],
 	argTypes: {
 		itemsPerPage: {
-			type: 'number'
+			type: 'number',
+			description: 'Quantity of items in each option of items per page',
+			defaultValue: 10
 		},
 		label: {
-			type: 'string'
+			type: 'string',
+			description: 'Description of items per page, the label.'
 		},
 		optionsQuantity: {
-			type: 'boolean'
+			type: 'boolean',
+			description: 'Quantity of options',
+			defaultValue: 3
 		},
 		value: {
-			type: 'number'
+			type: 'number',
+			description: 'Current value of ItemsPerPage component',
+			defaultValue: '10'
 		}
 	}
 } satisfies Meta<ItemsPerPage>;
@@ -26,5 +32,9 @@ export default meta;
 type Story = StoryObj<typeof meta>;
 
 export const Default: Story = {
-	args: {}
+	args: {
+		itemsPerPage: 10,
+		optionsQuantity: 3,
+		value: 20
+	}
 };
